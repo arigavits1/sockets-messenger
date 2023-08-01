433bf60a67c6dce777c37e94d509d8ba662436f3
arigavits1 <113630071+arigavits1@users.noreply.github.com>
2023-08-01 16:20:41 +0300

    Update utils.h

--- client/include/utils.h
+++ client/include/utils.h
@@ -1,73 +1,28 @@
 #pragma once
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <iostream>
+#include <functional>
+#include <thread>
+#include <atomic>
+#define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-#include "imgui.h"
-
-unsigned int WIN_WIDTH = 640;
-unsigned int WIN_HEIGHT = 500;
 
 namespace glfw
 {
-    GLFWwindow* window;
-    GLFWwindow* Setup()
-    {
-        if (!glfwInit())
-        {
-            std::cerr << "Failed to initialize glfw\n";
-            return NULL;
-        }
 
-        window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "ImGUI", NULL, NULL);
-        if (window == NULL)
-        {
-            std::cerr << "Failed to create window\n";
-            glfwTerminate();
-            return NULL;
-        }
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);
-
-        return window;
-    }
+    extern unsigned int WIN_WIDTH;
+    extern unsigned int WIN_HEIGHT;
+    extern GLFWwindow* window;
+    GLFWwindow* Setup();
 }
 
 namespace imgui
 {
-    ImGuiIO& Setup(GLFWwindow* window)
-    {
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
-        ImGui_ImplGlfw_InitForOpenGL(window, true);
-        ImGui_ImplOpenGL3_Init("#version 330");
-        return io;
-    }
-
-    void TextWindow(const char* name, std::string text, ImVec2 scale, ImVec2 position)
-    {
-        ImGui::SetNextWindowSize(scale);
-        ImGui::SetNextWindowPos(position);
-        ImGui::Begin("Messages", NULL, ImGuiWindowFlags_NoResize);
-        ImGui::TextUnformatted(text.c_str());
-        ImGui::End();
-    }
-
-    void InputWindow(const char* name, char* inputBuffer, size_t inputBufferSize, ImVec2 scale, ImVec2 position)
-    {
-        ImGui::SetNextWindowSize(scale);
-        ImGui::SetNextWindowPos(position);
-        ImGui::Begin("Prompt", NULL, ImGuiWindowFlags_NoResize);
-        ImGui::InputText("Enter message", inputBuffer, inputBufferSize, ImGuiInputTextFlags_EnterReturnsTrue);
-        ImGui::End();
-    }
+    ImGuiIO& Setup(GLFWwindow* window);
+    void TextWindow(const char* name, std::string text, ImVec2 scale, ImVec2 position);
+    void InputWindow(const char* name, char* inputBuffer, size_t inputBufferSize, ImVec2 scale, ImVec2 position);
 }
 
-void processInput(GLFWwindow* window, std::atomic<bool>& should_run)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    {
-        std::cout << "Escape\n";
-        should_run = false;
-        glfwSetWindowShouldClose(window, true);
-    }
-}
+void processInput(GLFWwindow* window, std::atomic<bool>& should_run);
